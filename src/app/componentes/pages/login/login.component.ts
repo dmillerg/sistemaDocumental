2fd287d65ecf42bdd7ab5a620935a11b3221a3b2
kuroko <dmillergg@gmail.com>
2022-02-25 13:00:48 -0500

    ajueste

--- src/app/componentes/pages/login/login.component.ts
+++ src/app/componentes/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { SessionStorageService } from 'ngx-webstorage';
 import { ApiService } from 'src/app/service/api.service';
 
@@ -14,26 +15,32 @@ export class LoginComponent implements OnInit {
   usuario_error: boolean = false;
   password_error: boolean = false;
   see: boolean = false;
+  error: string = '';
 
-  constructor(private api:ApiService, private storage:SessionStorageService) { }
+  constructor(private api: ApiService, private storage: SessionStorageService, private router: Router) { }
 
   ngOnInit(): void {
-  
+
   }
- loguear(){
-  if (this.usuario == '') {
-    this.usuario_error = true;
-  } else this.usuario_error = false;
-  if (this.password == '') {
-    this.password_error = true;
-  } else this.password_error = false;
-  var formData = new FormData();
-  formData.append('usuario', this.usuario);
-  formData.append('password', this.password);
-  this.api.login(formData).subscribe((result) => {
-    console.log(result);
-    this.storage.store('usuario', result);
-  });
- }
+  loguear() {
+    if (this.usuario == '') {
+      this.usuario_error = true;
+    } else this.usuario_error = false;
+    if (this.password == '') {
+      this.password_error = true;
+    } else this.password_error = false;
+    var formData = new FormData();
+    formData.append('usuario', this.usuario);
+    formData.append('password', this.password);
+    this.api.login(formData).subscribe((result) => {
+      console.log(result);
+      this.storage.store('usuario', result);
+      this.router.navigate(['inicio'])
+    },(error)=>{
+this.error = error.error.message;
+console.log(error.error.message);
+    }
+    );
+  }
 
 }
